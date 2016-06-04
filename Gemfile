1535d8ee7873e359a3e4388f9132427bda62caf3
JuanD <juand.herrera@hotmail.com>
2016-06-04 01:22:39 -0500

    Agregando recursos precompilados para Heroku con devise 2

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'materialize-sass'
 
 ##GEMS FOR HEROKU
 gem 'pg', '0.15.1'
+
 gem 'rails_12factor', '0.0.2'
 
 
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module MeetBike
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.initialize_on_precompile = false
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts", "font")
     config.assets.precompile += %w( .svg .eot .woff .ttf .woff .woff2)
